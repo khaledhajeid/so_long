759243649d00bf515926e4a2567481f1b3302485
Khaled Al-Haj Eid <kal-haj-@c1r5s13.42amman.com>
2025-10-26 19:12:10 +0300

    handle norm in 3 files

--- free.c
+++ free.c
@@ -6,7 +6,7 @@
 /*   By: kal-haj- <kal-haj-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 17:04:28 by kal-haj-          #+#    #+#             */
-/*   Updated: 2025/10/25 17:47:20 by kal-haj-         ###   ########.fr       */
+/*   Updated: 2025/10/26 18:45:59 by kal-haj-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,41 @@
 
 void	free_map(char **arr)
 {
-    int i;
+	int	i;
 
-    i = 0;
+	i = 0;
 	while (arr[i])
-    {
+	{
 		free(arr[i]);
-        i++;
-    }
+		i++;
+	}
 	free(arr);
 }
 
-void free_everything(t_game *game)
+void	free_everything(t_game *game)
 {
-    free_map(game->map);
-    if(game->texture.coin)
-        mlx_destroy_image(game->window.mlx, game->texture.coin);
-    if(game->texture.exit)
-        mlx_destroy_image(game->window.mlx, game->texture.exit);
-    if(game->texture.floor)
-        mlx_destroy_image(game->window.mlx, game->texture.floor);
-    if(game->texture.wall)
-        mlx_destroy_image(game->window.mlx, game->texture.wall);
-    if(game->texture.player[0])
-        mlx_destroy_image(game->window.mlx, game->texture.player[0]);
-    if(game->texture.player[1])
-        mlx_destroy_image(game->window.mlx, game->texture.player[1]);
-    if(game->texture.player[2])
-        mlx_destroy_image(game->window.mlx, game->texture.player[2]);
-    if(game->texture.player[3])
-        mlx_destroy_image(game->window.mlx, game->texture.player[3]);
-    if (game->window.win)
-        mlx_destroy_window(game->window.mlx, game->window.win);
-    if(game->window.mlx)
-    {
-        mlx_destroy_display(game->window.mlx);
-        free(game->window.mlx);
-    }
+	free_map(game->map);
+	if (game->texture.coin)
+		mlx_destroy_image(game->window.mlx, game->texture.coin);
+	if (game->texture.exit)
+		mlx_destroy_image(game->window.mlx, game->texture.exit);
+	if (game->texture.floor)
+		mlx_destroy_image(game->window.mlx, game->texture.floor);
+	if (game->texture.wall)
+		mlx_destroy_image(game->window.mlx, game->texture.wall);
+	if (game->texture.player[0])
+		mlx_destroy_image(game->window.mlx, game->texture.player[0]);
+	if (game->texture.player[1])
+		mlx_destroy_image(game->window.mlx, game->texture.player[1]);
+	if (game->texture.player[2])
+		mlx_destroy_image(game->window.mlx, game->texture.player[2]);
+	if (game->texture.player[3])
+		mlx_destroy_image(game->window.mlx, game->texture.player[3]);
+	if (game->window.win)
+		mlx_destroy_window(game->window.mlx, game->window.win);
+	if (game->window.mlx)
+	{
+		mlx_destroy_display(game->window.mlx);
+		free(game->window.mlx);
+	}
 }
-
--- read_texture.c
+++ read_texture.c
@@ -6,27 +6,44 @@
 /*   By: kal-haj- <kal-haj-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:24:11 by kal-haj-          #+#    #+#             */
-/*   Updated: 2025/10/25 17:42:13 by kal-haj-         ###   ########.fr       */
+/*   Updated: 2025/10/26 19:04:28 by kal-haj-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void read_texture(t_game *game)
+void	read_texture(t_game *game)
 {
-    game->texture.player[0] = mlx_xpm_file_to_image(game->window.mlx, "texture/player_up.xpm", &game->texture.width, &game->texture.height);
-    game->texture.player[1] = mlx_xpm_file_to_image(game->window.mlx, "texture/player_down.xpm", &game->texture.width, &game->texture.height);
-    game->texture.player[2] = mlx_xpm_file_to_image(game->window.mlx, "texture/player_left.xpm", &game->texture.width, &game->texture.height);
-    game->texture.player[3] = mlx_xpm_file_to_image(game->window.mlx, "texture/player_right.xpm", &game->texture.width, &game->texture.height);
-    game->texture.wall = mlx_xpm_file_to_image(game->window.mlx, "texture/wall.xpm", &game->texture.width, &game->texture.height);
-    game->texture.floor = mlx_xpm_file_to_image(game->window.mlx, "texture/floor.xpm", &game->texture.width, &game->texture.height);
-    game->texture.coin = mlx_xpm_file_to_image(game->window.mlx, "texture/coin.xpm", &game->texture.width, &game->texture.height);
-    game->texture.exit = mlx_xpm_file_to_image(game->window.mlx, "texture/exit.xpm", &game->texture.width, &game->texture.height);
-
-    if (!game->texture.player[0] || !game->texture.player[1] || !game->texture.player[2] || !game->texture.player[3] 
-        || !game->texture.wall || !game->texture.floor || !game->texture.coin || !game->texture.exit)
-    {
-        free_everything(game);
-        exit(0);
-    }
+	game->texture.player[0] = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/player_up.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.player[1] = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/player_down.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.player[2] = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/player_left.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.player[3] = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/player_right.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.wall = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/wall.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.floor = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/floor.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.coin = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/coin.xpm",
+			&game->texture.width, &game->texture.height);
+	game->texture.exit = mlx_xpm_file_to_image(game->window.mlx,
+			"texture/exit.xpm",
+			&game->texture.width, &game->texture.height);
+	if (!game->texture.player[0] || !game->texture.player[1]
+		|| !game->texture.player[2] || !game->texture.player[3]
+		|| !game->texture.wall || !game->texture.floor
+		|| !game->texture.coin || !game->texture.exit)
+	{
+		free_everything(game);
+		exit(0);
+	}
 }
